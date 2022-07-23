44fe3261d8d123ec41d69488c60db8449d2f958c
EnriqueRS <enrique.rios.santos@gmail.com>
2022-07-23 20:01:17 +0200

    Create service file

---
+++ src/services/toDoServicie.js
@@ -0,0 +1,27 @@
+const getAllToDos = () => {
+    return;
+}
+
+const getToDoById = () => {
+    return;
+}
+
+const createToDo = () => {
+    return;
+}
+
+const updateToDo = () => {
+    return;
+}
+
+const deleteToDo = () => {
+    return;
+}
+
+module.exports = {
+    getAllToDos,
+    getToDoById,
+    createToDo,
+    updateToDo,
+    deleteToDo
+}
