c23ce311faca0e7d63544a7829b377ffe4e986db
EnriqueRS <enrique.rios.santos@gmail.com>
2022-07-23 20:03:30 +0200

    toDoService rename

--- src/controllers/toDoController.js
+++ src/controllers/toDoController.js
@@ -1,20 +1,27 @@
+const toDoService = require('../../services/toDoService');
+
 const getAllToDos = (req, res) => {
+    const allToDos = toDoService.getAllToDos();
     res.send("Get all ToDos");
 };
 
 const getToDoById = (req, res) => {
+    const toDo = toDoService.getToDoById();
     res.send("Get an existing ToDo");
 }
 
 const createToDo = (req, res) => {
+    const newToDo = toDoService.createToDo();
     res.send("Create a new ToDo");
 }
 
 const updateToDo = (req, res) => {
+    const updatedToDo = toDoService.updateToDo();
     res.send("Update an existing ToDo");
 }
 
 const deleteToDo = (req, res) => {
+    toDoService.deleteToDo();
     res.send("Delete an existing ToDo");
 }
 
