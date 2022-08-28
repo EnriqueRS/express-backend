350616e9866392867f8f8555335994cb75801f22
EnriqueRS <enrique.rios.santos@gmail.com>
2022-08-28 18:29:00 +0200

    cors added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
 
@@ -12,6 +13,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000
 
+app.use(cors());
 app.use(function (req, res, next) {
     next();
 });
